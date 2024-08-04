10f8214c15bedb201449517eca2aa66bf724a96b
Javier <javier@Admins-MacBook-Air.local>
2024-08-04 15:50:39 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('chat-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+  
+    const gptUrl = 'https://chatgpt.com/g/g-xgl7diXqb-patience-biologia-y-geologia';
+  
+    window.location.href = gptUrl;
+  });  
